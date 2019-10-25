a0192e14cff6694627f0dffa18f75213cdf5a89f
your-name <your@email.com>
2019-10-25 11:10:35 -0500

    Created practice script

---
+++ Practice.R
@@ -0,0 +1,9 @@
+### Practicing R version control using github
+### Practice codes
+
+library("tidyverse")
+
+surveys_dot <- read.csv("C:/Users/Innwon Park/Documents/uw-r-workshops-master/uw-r-workshops-master/data/raw_surveys.csv")
+
+surveys_dot
+str(surveys_dot)
